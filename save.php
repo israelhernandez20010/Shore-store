ede6f9765d40d0cd9a847780420acc7daf6feee5
israelhernandez20010 <israelhernandez20010@gmail.com>
2024-10-13 14:36:32 +0800

    Delete save.php

--- save.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $content = $_POST['content'];
-    file_put_contents('info.txt', $content);
-    header('Location: lala.php');
-    exit;
-}
-?>
